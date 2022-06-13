e4447d43399f422e3fb14be47cf971345bf9dc5d
akshay ghanwat <ghanwatakshay@gmail.com>
2022-06-13 15:57:29 +0530

    to check

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+from calculator import sum,sub,multiply
+
+print(f"sum result={sum(5,6)}")
+print(f"sub result={sub(5,6)}")
+print(f"multiply result={multiply(5,6)}")
+
+
 print("welcome to git")
+
+print("here is someo text")
+
+print("you can define here anything")
