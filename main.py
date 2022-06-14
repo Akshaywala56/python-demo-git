03a5cd6e755aa100f1798f058b82d9bf6b86416e
akshay ghanwat <ghanwatakshay@gmail.com>
2022-06-14 10:10:06 +0530

    messenger added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from calculator import sum,sub,multiply
+from messenger import greet
 
 print(f"sum result={sum(5,6)}")
 print(f"sub result={sub(5,6)}")
@@ -10,3 +11,5 @@ print("welcome to git")
 print("here is someo text")
 
 print("you can define here anything")
+
+print(greet("akshay"))
